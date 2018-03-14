cdd5968d4f9e26946c765b9c51e9820d38608857
danialdo95 <danialdo95@gmail.com>
2018-03-14 18:46:51 +0800

    add new audio source and audio manager script and a low poly model

---
+++ Assets/Script/AudioScript/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+	// Use this for initialization
+	void Awake () {
+       
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
