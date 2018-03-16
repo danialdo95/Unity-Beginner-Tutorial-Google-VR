87dae7edf7222007415ca1123423710c492f6111
danialdo95 <danialdo95@gmail.com>
2018-03-16 17:57:00 +0800

    Fixed UI Menu

---
+++ Assets/Script/VRYesPlease.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VRYesPlease : MonoBehaviour {
+
+	
+	public void Start () {
+        StartCoroutine(ActivatorVR("cardboard"));
+	}
+
+    public IEnumerator ActivatorVR(string YESVR)
+    {
+        XRSettings.LoadDeviceByName(YESVR);
+        yield return null;
+        XRSettings.enabled = true;
+    }
+
+
+	
+	
+}
